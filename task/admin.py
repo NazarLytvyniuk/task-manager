3ca21d13db553f05915135f9ca3573f613e6025d
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-08-10 22:22:59 +0200

    Register models
    Register Worker, Task, TaskType and Position models. Change list_display, fieldsets and add_fieldsets in WorkerAdmin

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from task.models import Worker, TaskType, Task, Position
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position", )}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {"fields": ("position", "first_name", "last_name")}
+        ),
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
