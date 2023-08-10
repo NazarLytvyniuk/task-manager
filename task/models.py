8269ac80f34edc8210dde09b47cd9b79615d64f3
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-08-10 17:41:27 +0200

    Create model Task in models.py

--- task/models.py
+++ task/models.py
@@ -22,3 +22,23 @@ class Worker(AbstractUser):
     class Meta:
         verbose_name = "worker"
         verbose_name_plural = "workers"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("1", "High Priority"),
+        ("2", "Medium Priority"),
+        ("3", "Low Priority")
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        default="3"
+    )
+    task_type = models.ForeignKey(to=TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(to=Worker, related_name="tasks")
