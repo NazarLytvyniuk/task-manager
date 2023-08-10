648a0e8cd0d942cf057ee087c32fbcffc1ac70c1
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-08-10 17:44:49 +0200

    Specify the 'AUTH_USER_MODEL' and add 'task' to installed_apps in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "task"
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "task.Worker"
 
 
 # Internationalization
