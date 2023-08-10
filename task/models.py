c037774d9207c0db84ac46370948d5fb34aae136
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-08-10 17:24:25 +0200

    Add Woker model to models.py

--- task/models.py
+++ task/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -13,3 +14,11 @@ class TaskType(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(to=TaskType, on_delete=models.CASCADE,)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
