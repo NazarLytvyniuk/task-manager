653b6bce55caba5e4c10cf3230644db8c4ac992f
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-08-13 22:37:43 +0200

    Add view for home page

--- task/views.py
+++ task/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from task.models import Task
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    context = {
+        "workers": get_user_model().objects.count(),
+        "tasks": Task.objects.count()
+    }
+
+    return render(request, "task/index.html", context=context)
